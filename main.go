35ed58a5d329005384d59d5364fa1ba27286006e
nelabdiel <nel.abdiel@gmail.com>
2017-06-11 19:52:29 -0400

    few changes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ import (
     "fmt"
     "os"
     "encoding/json"
+    "net/url"
     "github.com/ChimeraCoder/anaconda"
 )
 
@@ -13,22 +14,23 @@ var secrets struct {
     AccessTokenSecret string `json:"accessTokenSecret"`
 }
 
-
-
 func main() {
-    configFile, err := os.Open("../variables.json")
+    twitterSecrets, err := os.Open("../variables.json")
     if err != nil {
         fmt.Println(err.Error())
     }
 
-    jsonParser := json.NewDecoder(configFile)
+    jsonParser := json.NewDecoder(twitterSecrets)
     if err = jsonParser.Decode(&secrets); err != nil {
         fmt.Println(err.Error())
     }
     anaconda.SetConsumerKey(secrets.ApiKey)
     anaconda.SetConsumerSecret(secrets.ApiSecret)
     api := anaconda.NewTwitterApi(secrets.AccessToken,secrets.AccessTokenSecret)
-    searchResult, _ := api.GetSearch("charleston AND event", nil) 
-    for _ , tweet := range searchResult.Statuses {fmt.Println(tweet.Text)}
+
+    v := url.Values{}
+    v.Set("count", "30")
+    result, err := api.GetSearch("charleston AND event -RT", v)
+    for _, tweet := range result.Statuses {fmt.Println(tweet.Text)}
 }
 
