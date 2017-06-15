f8d8a7c8a0d06440fc464d32a4433c16d68f6a35
nelabdiel <nel.abdiel@gmail.com>
2017-06-14 21:34:33 -0400

    added some comments

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
     "github.com/ChimeraCoder/anaconda"
 )
 
+//Twitter credentials
 var secrets struct {
     ApiKey  string  `json:"apiKey"`
     ApiSecret  string `json:"apiSecret"`
@@ -15,8 +16,10 @@ var secrets struct {
     AccessTokenSecret string `json:"accessTokenSecret"`
 }
 
+//Main function that extracts links from tweets.
 func main() {
-    twitterSecrets, err := os.Open("../variables.json")
+	//Opening json with twitter credentials.
+    twitterSecrets, err := os.Open("/Users/Nel/Desktop/Code/go/variables.json")
     if err != nil {
         fmt.Println(err.Error())
     }
@@ -25,17 +28,22 @@ func main() {
     if err = jsonParser.Decode(&secrets); err != nil {
         fmt.Println(err.Error())
     }
+	//Loading credentials and setting up credentials.
     anaconda.SetConsumerKey(secrets.ApiKey)
     anaconda.SetConsumerSecret(secrets.ApiSecret)
     api := anaconda.NewTwitterApi(secrets.AccessToken,secrets.AccessTokenSecret)
 
     v := url.Values{}
     v.Set("count", "30")
+	//Search for tweets that contain charleston and event.
     result, err := api.GetSearch("charleston AND event -RT", v)
+	//Go through tweets
     for _, tweet := range result.Statuses {
-	for _, link := range strings.Split(tweet.Text, " ") {
+		//Check if there is a link in tweet.
+		for _, link := range strings.Split(tweet.Text, " ") {
             hasLink := strings.Index(link, "http") == 0
             if hasLink {
+				//Print link.
                 fmt.Println(link)
             }
         }
